9898f46bea3d0267f99cfa5c920f6c6a10af0ef0
Kejdi <kejdimuci.dev@gmail.com>
2025-08-13 22:15:55 +0200

    Adding TestChatUser and ChatUser classes

---
+++ src/Homework/ChatUser.java
@@ -0,0 +1,55 @@
+package Homework;
+
+public class ChatUser {
+    private String nickname, location;
+    private int age;
+    private static int number = 1;
+
+    ChatUser(){
+        nickname = "Anonymous" + number++;
+        age = 20;
+        location = "U.S.A";
+        System.out.println("User " + nickname + " age:" + age +" location: " + location + " joined the chat.");
+    }
+
+    ChatUser(int age){
+        nickname = "Anonymous" + number++;
+        this.age = age;
+        location = "U.S.A";
+        System.out.println("User " + nickname + " age:" + age +" location: " + location + " joined the chat.");
+    }
+
+    ChatUser(String nickname, int age){
+        this.nickname = nickname;
+        this.age = age;
+        location = "U.S.A";
+        System.out.println("User " + nickname + " age:" + age +" location: " + location + " joined the chat.");
+    }
+
+    ChatUser(String nickname, int age, String location){
+        this.nickname = nickname;
+        this.age = age;
+        this.location = location;
+        System.out.println("User " + nickname + " age:" + age +" location: " + location + " joined the chat.");
+    }
+
+    public String getNickname(){
+        return nickname;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setLocation(String Location){
+        this.location = location;
+    }
+
+    public void printStatus(){
+        System.out.println("User " + nickname + " age: " + age +" location: " + location + " is the chat.");
+    }
+}
---
+++ src/Homework/TestChatUser.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class TestChatUser {
+    public static void main(String[]args){
+        ChatUser u1 = new ChatUser("JonathanD",31);
+        ChatUser u2 = new ChatUser();
+        ChatUser u3 = new ChatUser("Antonette",24,"France");
+        ChatUser u4 = new ChatUser(36);
+        u1.setLocation("Canada");
+        u2.setLocation("Japan");
+        u1.printStatus();
+        u2.printStatus();
+        u3.printStatus();
+        u4.printStatus();
+    }
+}
